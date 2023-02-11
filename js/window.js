403f83e5746a6326e02c6503ea66136fb4f666fa
akishtp <makishtp@gmail.com>
2023-02-12 00:34:56 +0530

    dadded some icons

--- js/window.js
+++ js/window.js
@@ -48,20 +48,85 @@ function dragElement(elmnt) {
 // to resize window
 function makeResizableDiv(div) {
   const element = document.querySelector(div);
-  const resizers = document.querySelectorAll(div + " .resizer");
+  const resizers = document.querySelectorAll(div + " .corner-resizer");
+  const minimum_size = 20;
+  const minimum_width = 180;
+  let original_width = 0;
+  let original_height = 0;
+  let original_x = 0;
+  let original_y = 0;
+  let original_mouse_x = 0;
+  let original_mouse_y = 0;
   for (let i = 0; i < resizers.length; i++) {
     const currentResizer = resizers[i];
     currentResizer.addEventListener("mousedown", function (e) {
-      currentResizer.addEventListener("mousemove", resize);
+      e.preventDefault();
+      original_width = parseFloat(
+        getComputedStyle(element, null)
+          .getPropertyValue("width")
+          .replace("px", "")
+      );
+      original_height = parseFloat(
+        getComputedStyle(element, null)
+          .getPropertyValue("height")
+          .replace("px", "")
+      );
+      original_x = element.getBoundingClientRect().left;
+      original_y = element.getBoundingClientRect().top;
+      original_mouse_x = e.pageX;
+      original_mouse_y = e.pageY;
+      window.addEventListener("mousemove", resize);
+      window.addEventListener("mouseup", stopResize);
     });
 
     function resize(e) {
       if (currentResizer.classList.contains("bottom-right")) {
-        element.style.width =
-          e.pageX - element.getBoundingClientRect().left + "px";
+        const width = original_width + (e.pageX - original_mouse_x);
+        const height = original_height + (e.pageY - original_mouse_y);
+        if (width > minimum_width) {
+          element.style.width = width + "px";
+        }
+        if (height > minimum_size) {
+          element.style.height = height + "px";
+        }
+      } else if (currentResizer.classList.contains("bottom-left")) {
+        const height = original_height + (e.pageY - original_mouse_y);
+        const width = original_width - (e.pageX - original_mouse_x);
+        if (height > minimum_width) {
+          element.style.height = height + "px";
+        }
+        if (width > minimum_size) {
+          element.style.width = width + "px";
+          element.style.left = original_x + (e.pageX - original_mouse_x) + "px";
+        }
+      } else if (currentResizer.classList.contains("top-right")) {
+        const width = original_width + (e.pageX - original_mouse_x);
+        const height = original_height - (e.pageY - original_mouse_y);
+        if (width > minimum_width) {
+          element.style.width = width + "px";
+        }
+        if (height > minimum_size) {
+          element.style.height = height + "px";
+          element.style.top = original_y + (e.pageY - original_mouse_y) + "px";
+        }
+      } else {
+        const width = original_width - (e.pageX - original_mouse_x);
+        const height = original_height - (e.pageY - original_mouse_y);
+        if (width > minimum_width) {
+          element.style.width = width + "px";
+          element.style.left = original_x + (e.pageX - original_mouse_x) + "px";
+        }
+        if (height > minimum_size) {
+          element.style.height = height + "px";
+          element.style.top = original_y + (e.pageY - original_mouse_y) + "px";
+        }
       }
+    }
+
+    function stopResize() {
+      window.removeEventListener("mousemove", resize);
     }
   }
 }
 
-makeResizableDiv("#window");
+makeResizableDiv(".window");
