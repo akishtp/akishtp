e4924a0001b368d3c1898544f9f2fc9f0b1047a6
akishtp <makishtp@gmail.com>
2023-03-12 23:20:06 +0530

    js works. sometimes

---
+++ js/js.js
@@ -0,0 +1,13 @@
+// document.addEventListener("click", function (event) {
+//   if (event.target.classList.contains("nav-item")) {
+//     event.target.style.height = "100vh";
+//   }
+// });
+
+const divElement = document.querySelector(".moving-text");
+divElement.addEventListener("click", function () {
+  // Get the parent div element
+  const parentDiv = this.parentElement;
+  // Change the style of the parent div element
+  parentDiv.style.backgroundColor = "red";
+});
