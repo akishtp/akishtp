4a97dae5bcbfc680c70fe80205c1f7e75ba91aa4
akishtp <makishtp@gmail.com>
2023-03-14 19:00:20 +0530

    ella bhakathum work aavind 😮‍💨

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ document.addEventListener("click", function (event) {
     const y = parentDiv.offsetTop;
     console.log(y);
     parentDiv.style.cssText =
-      "width:100vw; position: absolute; border-top: 0.4vw solid #000; height: 100vh; transition: background-color 10s ease-in; background-color: #dde7f3; transition: height 1.6s ease-in-out, background-color 2s ease-in-out, transform 1.6s ease-in-out;";
+      "width:100vw; border-top: 0.4vw solid #000; height: 66vw; transition: background-color 10s ease-in; background-color: #dde7f3; transition: height 1.6s ease-in-out, background-color 2s ease-in-out, transform 1.6s ease-in-out; z-index: 99;";
     parentDiv.style.transform = `translateY(-${y}px)`;
     const childElements = parentDiv.querySelectorAll("*");
     childElements.forEach(function (childElement) {
