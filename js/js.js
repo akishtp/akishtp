743f061a1fc30644b71709b69153107018df20b3
akishtp <makishtp@gmail.com>
2023-03-14 20:00:25 +0530

    href links 🔗

--- js/js.js
+++ js/js.js
@@ -1,14 +1,18 @@
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("moving-text")) {
+    event.preventDefault();
     const parentDiv = event.target.parentElement;
+    const targetUrl = parentDiv.href;
     const y = parentDiv.offsetTop;
-    console.log(y);
     parentDiv.style.cssText =
-      "width:100vw; border-top: 0.4vw solid #000; height: 66vw; transition: background-color 10s ease-in; background-color: #dde7f3; transition: height 1.6s ease-in-out, background-color 2s ease-in-out, transform 1.6s ease-in-out; z-index: 99;";
+      "width:100vw; border-top: 0.4vw solid #000; height: 66vw; transition: background-color 10s ease-in; background-color: #dde7f3; transition: height 1s ease-in-out, background-color 1.6s ease-in-out, transform 1.6s ease-in-out; z-index: 99;";
     parentDiv.style.transform = `translateY(-${y}px)`;
     const childElements = parentDiv.querySelectorAll("*");
     childElements.forEach(function (childElement) {
       childElement.style.display = "none";
     });
+    setTimeout(() => {
+      window.location.href = targetUrl; // transition to the new page
+    }, 2000);
   }
 });
