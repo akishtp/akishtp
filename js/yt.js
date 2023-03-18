20cb97220eab8a6a489920b9c4eb4df608e43586
akishtp <makishtp@gmail.com>
2023-03-18 21:03:56 +0530

    fetch channel image and sub count

--- js/yt.js
+++ js/yt.js
@@ -2,15 +2,14 @@ const channelId = "UCO24ZZpXZlp1fJhtE93aOsw";
 const apiKey = "AIzaSyBWhedNatmmDNjBLrC0jRfjBwUcmFwDKC8";
 const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
 const url2 = `https://www.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${channelId}&key=${apiKey}`;
-const url3 = `https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id=${channelId}&key=${apiKey}`;
 
 const channelThumbnailEl = document.querySelector(".channel-thumbnail");
 const subscribersEl = document.querySelector(".subscribers");
 
-fetch(url3)
+fetch(url2)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
     // const subscriberCount = channelData.statistics.subscriberCount;
     // const thumbnailUrl = channelData.snippet.thumbnails.default.url;
     // console.log(channelData);
@@ -19,5 +18,24 @@ fetch(url3)
   })
   .catch((error) => {
     console.error(error);
-    subscribersEl.innerHTML = "Error fetching channel data";
+    subscribersEl.innerHTML = "Subscribers: Can't fetch. Check channel.";
+  });
+
+// fetch channel banner
+fetch(url2)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.kind);
+  });
+
+// fetch channel image
+const url3 = `https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails%2Fdefault&id=${channelId}&key=${apiKey}`;
+fetch(url3)
+  .then((response) => response.json())
+  .then((data) => {
+    const imageUrl = data.items[0].snippet.thumbnails.default.url;
+    const channelImage = document.querySelector(".channel-image");
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    channelImage.appendChild(image);
   });
