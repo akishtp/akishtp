36fcf014ab2323a6495ca62459c6453cddffcab9
akishtp <makishtp@gmail.com>
2023-03-19 19:25:37 +0530

    mobile looks a bit better

--- js/yt.js
+++ js/yt.js
@@ -23,9 +23,7 @@ fetch(url)
 fetch(url2)
   .then((response) => response.json())
   .then((data) => {
-    const imageUrl =
-      data.items[0].brandingSettings.image.bannerExternalUrl +
-      "=w2120-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj";
+    const imageUrl = data.items[0].brandingSettings.image.bannerExternalUrl;
     const channelImage = document.querySelector(".banner");
     const image = document.createElement("img");
     image.src = imageUrl;
