a4cc05e089d7e985ff1502b448608dd2d84fdb43
akishtp <makishtp@gmail.com>
2023-08-08 21:07:32 +0530

    youtube redo why I do?

---
+++ js/yy.js
@@ -0,0 +1,3 @@
+const channelId = "UCO24ZZpXZlp1fJhtE93aOsw";
+const apiKey = "AIzaSyBWhedNatmmDNjBLrC0jRfjBwUcmFwDKC8";
+const url_thumbnail = `https://www.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${channelId}&key=${apiKey}`;
