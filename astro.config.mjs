88ed3b7f49727720f10ee28c41896c48dd6aa58c
Akish tp <makishtp@gmail.com>
2024-01-28 11:28:43 +0530

    Domain setup not complete?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://akishtp.github.io",
-  base: "/akishtp",
+  site: "https://akish.tech",
   integrations: [tailwind()],
 });
