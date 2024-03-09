c93d9227853898810df3f777d7fec0211635e0f8
Akish tp <makishtp@gmail.com>
2024-03-09 20:50:41 +0530

    init v2.1 2.0

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,48 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html}"],
-  theme: {
-    extend: {
-      height: {
-        "20vh": "20vh",
-        "40vh": "40vh",
-      },
-      colors: {
-        primary: "#70a0ff",
-        "primary-hover": "#2481de",
-        projects: "#c86fff",
-        "projects-hover": "#9d00ff",
-        gallery: "#fffa6f",
-        "gallery-hover": "#ffd51b",
-      },
-      fontSize: {
-        hero: [
-          "30vh",
-          {
-            lineHeight: "20vh",
-            letterSpacing: "-0.8rem",
-            fontWeight: "700",
-          },
-        ],
-        "hero-selected": [
-          "20vh",
-          {
-            lineHeight: "16vh",
-            letterSpacing: "-0.4rem",
-            fontWeight: "700",
-          },
-        ],
-      },
-      keyframes: {
-        marquee: {
-          "0%": { left: "100%" },
-          "100%": { left: "-100%" },
-        },
-      },
-      animation: {
-        marquee: "marquee 24s linear infinite",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
