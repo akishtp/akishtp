b6e10a1d9e2660604df1f71991cb490bb8e7d8fe
Akish T P <makishtp@gmail.com>
2024-07-05 23:25:07 +0530

    only writings left to design. how should i do it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://akish.tech",
-  integrations: [tailwind()],
-});
+export default defineConfig({});
